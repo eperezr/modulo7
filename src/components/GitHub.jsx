b3f1cdd64bfa8ca1c380ced9fd1d3a195aabd37b
Eddy Perez <eddyperez007@hotmail.com>
2024-08-19 23:34:18 -0400

    test

---
+++ src/components/GitHub.jsx
@@ -0,0 +1,17 @@
+const Content = () => {
+    return (
+        <div>
+            <a
+                className="App-link"
+                href="https://github.com/eperezr/modulo7.git"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+
+                Mi Proyecto React (GitHub)
+            </a>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/screens/Default.jsx
@@ -0,0 +1,18 @@
+import Logo from '../components/Logo.jsx';
+import Description from '../components/Description.jsx';
+import Content from '../components/Content.jsx';
+import GitHub from '../components/GitHub.jsx';
+
+const Default = () => {
+    return (
+        <header className="App-header">
+            <Logo />
+            <Description />
+            <Content />
+            <GitHub />
+         </header>
+    );
+
+};
+
+export default Default
